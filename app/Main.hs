b74fc617fa52200dcdcc6992523a8a6808e60d5c
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2020-07-02 17:06:14 +0200

    Finishing Program

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,16 @@ module Main where
 import           Data.Aeson
 import           Data.Aeson.Types
 import           Data.Yaml.Config
+import           GHC.Show
 import           Options.Applicative as Opt
 import           Options.Generic
-import           Protolude
+import           Protolude           as P
 import           Simulation
 
 data Customer = Yellow
               | Blue
               | Red
-              deriving (Generic, Read)
+              deriving (Generic, Read, Show)
 
 instance ParseField Customer
 instance ParseFields Customer
@@ -39,10 +40,22 @@ data ConfigSelection = ConfigSelection
   , showMaxWait  :: Bool
   , showMaxQueue :: Bool
   , showDiff     :: Bool
+  , showAll      :: Bool
   } deriving (Generic, Read)
 
 instance ParseField ConfigSelection
 instance ParseRecord ConfigSelection
+
+instance Show ConfigSelection where
+  show ConfigSelection{..} =
+    "\n------- BANK CONFIG SELECTION ------ " <>
+    "\nCustomer Type: " <> P.show custType <>
+    "\nShow Average Wait Time: " <> P.show showAvgWait <>
+    "\nShow Average Queue Length: " <> P.show showAvgQueue <>
+    "\nShow Maximum Wait Time: " <> P.show showMaxWait <>
+    "\nShow Maximum Queue Length: " <> P.show showMaxQueue <>
+    "\nShow Diff between Average and Maximum: " <> P.show showDiff <>
+    "\n------------------------------------ "
 
 toCustomerConfig :: Customer -> Object -> Data.Aeson.Types.Parser CustomerConfig
 toCustomerConfig cust =
@@ -71,11 +84,26 @@ mapToLibConf ConfigSelection{..} Config {..} =
                       Blue   -> fromCustTypeToBeta blue
    in SimConfig maxRunTime betaDist (Exponential alphaExpDist)
 
+allConfigs :: [(ConfigSelection -> Bool, Text, SimSystem -> Integer)]
+allConfigs = [(showAvgWait, "\nAverage Waiting Time: ", averageWaitingTime)
+            , (showAvgQueue, "\nAverage Queue Size: ", averageQueueSize)
+            , (showMaxWait, "\nMax Waiting Time: ",  maxWaitTime)
+            , (showMaxQueue, "\nMax Queue Size: ",  maxQueueSize)
+            , (showDiff, "\nDiff Waiting Time: ", diffWaitingTime)]
+
+showResults :: ConfigSelection -> SimSystem -> IO ()
+showResults conf sis = do
+  configs <- filterM (\(b, _, _) -> pure $ showAll conf || b conf) allConfigs
+  putText "\n--------- BANK SIMULATION RESULT ----------"
+  putText $ foldMap (\(_, t, f) -> t <> (P.show $ f sis)) configs
+  putText "\n-------------------------------------------"
+
 main :: IO ()
 main = do
-  putText "----- BANK SIMULATION PROGRAM ------"
+  putText "\n----- BANK SIMULATION PROGRAM ------\n"
   config <- getRecord "Bank Simulation Program"
-  readFileConf (fileConfig config)
-    >>= pure .  mapToLibConf config
-    >>= executeSimulation
-    >>= print . averageWaitingTime
+  putText $ P.show config
+  confSim <- readFileConf (fileConfig config) >>= pure . mapToLibConf config
+  putText $ P.show confSim
+  result <- executeSimulation confSim
+  showResults config result
--- src/Internal/Config.hs
+++ src/Internal/Config.hs
@@ -1,14 +1,23 @@
 module Internal.Config where
 
 import           Data.Statistics.Distributions
+import           GHC.Show
 import           Lens.Micro.TH
-import           Protolude
+import           Protolude                     as P
 
 data SimConfig = SimConfig
   { _maxRunningTime   :: Integer
   , _betaDistribution :: Beta
   , _expDistribution  :: Exponential
-  } deriving Show
+  }
+
+instance Show SimConfig where
+  show SimConfig{..} =
+    "\n ----- SIMULATION CONFIG ------ " <>
+    "\nMax Running Time in Sec: " <> (P.show _maxRunningTime) <>
+    "\nBeta Distribution: \\alpha[" <> (P.show $ _alphaB _betaDistribution) <> "] - \\beta[" <> (P.show $ _beta _betaDistribution) <> "]" <>
+    "\nExponential Distribution: \\alpha[" <> (P.show $ _alphaE _expDistribution) <> "]" <>
+    "\n ------------------------------ "
 
 makeLenses ''SimConfig
 
